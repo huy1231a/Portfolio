eb5affd423c1db231afe9636c324256dac2df078
nQHuy2 <67337070+huy1231a@users.noreply.github.com>
2024-04-06 10:13:23 +0000

    fix tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+
 import Nav from '@/Components/Nav'
 import { useState } from 'react'
 import NavMobile from '@/Components/NavMobile'
 import Hero from '@/Components/Hero'
 import About from '@/Components/About'
-
-const inter = Inter({ subsets: ['latin'] })
+ 
 
 export default function Home() {
   const [nav, setNav] = useState<boolean>(false)
@@ -23,7 +21,7 @@ export default function Home() {
   return (
     <div className='overflow-x-hidden'>
       <div>
-        <NavMobile nav={nav} closeNav={closeNav} />
+      <NavMobile nav={nav} closeNav={closeNav} />
         <Nav openNav={openNav} />
         <Hero />
         <div className='relative z-[30]'>
